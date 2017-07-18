9f297032c33782e6d295c821bb99e8ba8d6d998c
Niranjan P B <niranjanpb03@gmail.com>
2017-07-18 21:11:43 +0530

    Fragments and phone login

--- app/src/main/java/com/example/pathrer/stolx/SelectionActivity.java
+++ app/src/main/java/com/example/pathrer/stolx/SelectionActivity.java
@@ -1,5 +1,6 @@
 package com.example.pathrer.stolx;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        SelectFragment fragment = new SelectFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame,fragment,"fragment");
+        fragmentTransaction.commit();
     }
 }
